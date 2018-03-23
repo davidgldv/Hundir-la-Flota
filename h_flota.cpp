07a5ea31be908412854c49b22ceb8fcb1edd064a
DanielVillalobosHdez <danivillalobos1997@gmail.com>
2018-03-23 14:04:09 +0100

    Update

--- h_flota.cpp
+++ h_flota.cpp
@@ -5,24 +5,63 @@
 
 #define N 25
 
-void j1_colocar();
+void interfaz();
+void coor_j1();
 
 int m_original[N][N];
+int x;
+int y;
 
 int main(){
-	system("clear");
-	printf("\n");
-	printf("\t\t\t");
-	for(int f = 0; f < N; f++){
-		for(int c = 0; c < N; c++){
+
+	for(int f = 0; f < N; f++)
+		for(int c = 0; c < N; c++)
 			m_original[f][c] = 0;
-			printf("%i ", m_original[f][c]);
+		
+
+
+		interfaz();
+
+		return EXIT_SUCCESS;
+	}
+
+	void interfaz() {
+		system("clear");
+		printf("\n");
+		printf("\t\t\t");
+		for(int f = 0; f < N; f++){
+			if(f <= 9 )
+				printf("|%i ", f);
+			else
+				printf("|%i", f);
 		}
 		printf("\n");
 		printf("\t\t\t");
-	}
-	sleep(3);
-	system("clear");
+		for(int f = 0; f < N; f++)
+			printf("|__");
+		printf("\n");
+		printf("\t\t     ");
+		for(int f = 0; f < N; f++){
+			if( f <= 9)
+				printf(" %i_|", f);
+			else
+				printf(" %i|", f);
+			for(int c = 0; c < N; c++){
+				printf("%i_|", m_original[f][c]);
+			}
+			printf("\n");
+			printf("\t\t     ");
+		}
 
-	return EXIT_SUCCESS;
-}
+		coor_j1();
+
+	}
+
+	void coor_j1 () {
+
+		printf("Coordenadas (Formato: (X,Y)): ");
+		scanf(" (%i,%i)", &x, &y);
+
+		m_original[x][y] = 1;
+		interfaz();
+	}
