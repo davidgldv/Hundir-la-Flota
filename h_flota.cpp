672c49c12112e98ec17a51ca3859a1744e435f61
DanielVillalobosHdez <danivillalobos1997@gmail.com>
2018-03-23 12:35:17 +0100

    hundir la flota

--- h_flota.cpp
+++ h_flota.cpp
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 #define N 25
 
 void j1_colocar();
 
+int m_original[N][N];
 
 int main(){
+	system("clear");
+	printf("\n");
+	printf("\t\t\t");
+	for(int f = 0; f < N; f++){
+		for(int c = 0; c < N; c++){
+			m_original[f][c] = 0;
+			printf("%i ", m_original[f][c]);
+		}
+		printf("\n");
+		printf("\t\t\t");
+	}
+	sleep(3);
+	system("clear");
 
-    for(int f = 0; f < N; f++){
-        for(int c = 0; c < N; c++){
-            printf("|_|");
-          }
-        printf("\n");
-    }
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
